663076d6b8215db961962a17e11414271af1b6ea
Jonah Raider Roth <jer135@case.edu>
2015-10-20 00:25:36 -0400

    Fixed Runner class for people who are not me

--- src/Runner.java
+++ src/Runner.java
@@ -7,7 +7,7 @@ public class Runner {
 
     public static void main(String[] args) {
         StringGraph graph = new StringGraph();
-        graph.importText("/Users/jonahrr/Desktop/hp1.txt");
+        graph.importText("/texts/Earnest.txt");
         System.out.println(graph.buildChain(10));
     }
 
